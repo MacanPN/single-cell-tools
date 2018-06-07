85ac511ccddc0d9c15af952a4b1e319cc6fcc64d
cobriniklab <kstachelek@chla.usc.edu>
2018-06-07 10:09:57 -0700

    pandas reindex doesn't accept axis argument

--- sc_pseudotime.py
+++ sc_pseudotime.py
@@ -471,8 +471,10 @@ def plot_3d_pca(transformed_expression, annotation, settings, expression_table=N
 			trx = gene_info['ensembl']['transcript']
 		#test = all(elem in test for elem in expression_table.columns)
 		if type(trx) == list:
-			sub_trx = expression_table.reindex(trx, axis = 1)
-			sum_trx = sub_trx.loc[:,trx].sum(axis=1)
+			sum_trx = expression_table.loc[:,trx]
+			# catch pandas omission of missing list values in loc; incompatible between pandas versions
+			#~ sub_trx = expression_table.reindex(trx, axis = 1)
+			#~ sum_trx = sub_trx.loc[:,trx].sum(axis=1)
 		else:
 			sum_trx = expression_table.loc[:,trx]
 		
