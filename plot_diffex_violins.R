8d34ee231a07c2ee45f917d4f6e2d0b83b376571
cobriniklab <kstachelek@chla.usc.edu>
2018-06-19 11:34:23 -0700

    add plot settings and cell settings to violin plots

--- plot_diffex_violins.R
+++ plot_diffex_violins.R
@@ -2,15 +2,22 @@
 
 suppressMessages(library(optparse))
 
-
-default_expr_mat = "~/single_cell_pipeline/output/FACS_20170407_sunlee_H_sapiens_output/sunhye_census_matrix_20170407.csv"
-default_annotation = "~/single_cell_pipeline/scde_input/shl_0407_w_centroids_cell_info.csv"
-
 # default_expr_mat = "~/single_cell_pipeline/output/FACS_20171031_sunlee_H_sapiens_output/transcripts.tpm_census_matrix.csv"
 # default_annotation = "~/single_cell_pipeline/output/FACS_20171031_sunlee_H_sapiens_output/FACS_20171031_sunlee_sample_sheet.csv"
 
 # default_clusters = "~/single_cell_pipeline/scde_input/diffex_by_trs_clusters_1_4/"
-default_out = "./"
+
+#SHL 20171031
+# default_infile <- "~/single_cell_pipeline/output/FACS_20171031_sunlee_H_sapiens_output/FACS_20171031_sunlee_H_sapiens_summarized_experiment.rds"
+# default_cell_info = "~/single_cell_tools/FACS_0407_1031_SHL_input_files/cells_sets_1_2.csv"
+# default_out = "~/monocle_test.pdf"
+
+#SHL 20170407
+default_expr_mat = "~/single_cell_pipeline/output/FACS_20170407_sunlee_H_sapiens_output/sunhye_census_matrix_20170407.csv"
+default_annotation = "~/single_cell_pipeline/scde_input/shl_0407_w_centroids_cell_info.csv"
+default_cell_info <- "~/single_cell_tools/FACS_0407_2017_SHL_input_files/cell_sets_0407_SHL_20180523.csv"
+default_plot_settings <- "~/single_cell_tools/FACS_0407_2017_SHL_input_files/plot_setting_0407_SHL_20180212.csv"
+default_out = "/home/skevin"
 
 #'  section for parsing command line options when calling script
 #'  ###################################
@@ -19,6 +26,10 @@ option_list = list(
               help="gene expression input filename [default= %default]", metavar="character"),
   make_option(c("-a", "--annotation"), type="character", default=default_annotation,
               help="metadata about cells in input file [default= %default]", metavar="character"),
+  make_option(c("-c", "--cellset"), type="character", default=default_cell_info,
+              help="tab delimited cell settings file [default= %default]", metavar="character"),
+  make_option(c("-p", "--plot_settings"), type="character", default=default_plot_settings,
+              help="tab delimited plot settings file [default= %default]", metavar="character"), 
   make_option(c("-o", "--out"), type="character", default=NA,
               help="output file name [default= %default]", metavar="character")
 );
@@ -31,22 +42,146 @@ if (any(sapply(opt, is.na))){
   stop("Please provide all necessary arguments.", call.=FALSE)
 }
 
+# load required libraries -------------------------------------------------
+
 suppressMessages(library(tidyverse))
 suppressMessages(library(gtools))
 suppressMessages(library(EnsDb.Hsapiens.v86))
 edb <- EnsDb.Hsapiens.v86
 
-# make_option(c("-cl", "--clusters"), type="character", default=default_clusters,
-#             help="file that defines comparison groups [default= %default]", metavar="character"),
 
-genes_names <- c('733_234_transcripts_filtered.csv', '733_345_transcripts_filtered.csv', '737_transcripts_filtered.csv', 'ctrl_transcripts_filtered.csv')
+# load required functions -------------------------------------------------
 
-new_genes_names <- paste0(opt$clusters, genes_names)
 
-print("loading census matrix")
-census_matrix = cataract::safe_read(opt$expr_mat)
-print("loading cell metadata")
-annotation = cataract::safe_read(opt$annotation)
+find_remove_cells <- function(plot_settings, annotation){
+  
+  test <- readLines(plot_settings)
+  
+  if (!grepl('remove', test)){
+    return(NULL)
+  }
+  
+  vecs <- list()
+  mtnames <- c()
+  for (i in test){
+    if (!grepl("#", i) & grepl('remove', i)){
+      lbline = strsplit(i, "\t")
+      d = unlist(lbline)[[2]]
+      vecs <- append(vecs, lbline)
+      mtnames <- append(mtnames, d)    
+    }
+  }
+  
+  pfx <- tolower(gsub("_.*", "", mtnames))
+  valid_pfx  <- which(pfx %in% colnames(annotation))
+  
+  if (length(valid_pfx) == 0){
+    return(NULL)
+  }
+  
+  pfx <- pfx[valid_pfx]
+  
+  
+  
+  sfx <- gsub(".*_", "", mtnames[valid_pfx])
+  
+  remove_cells <- purrr::map2(pfx, sfx , function(x, y) annotation[annotation[tolower(x)] == y,])
+  
+  
+  
+  remove_cells <- dplyr::bind_rows(remove_cells)
+  
+  ind <- apply(remove_cells, 1, function(x) all(is.na(x)))
+  remove_cells <- remove_cells[ !ind, ]
+  
+  remove_cells <- unique(remove_cells[,1])
+  
+}
+
+
+match_cols <- function(match_vecs, sv_name){
+  out=NULL
+  for (i in match_vecs){
+    vetor <- i
+    vetor <- vetor[vetor != ""]
+    key <- data.frame(sample_id=vetor[-1], sv_name=rep(gsub(".*_","", vetor[[1]]), (length(vetor)-1)))  
+    out <- rbind(out, key)
+  }  
+  colnames(out) <- c("sample_id", sv_name) 
+  return(out)
+}
+
+convert_mt_setting <- function(cell_settings, plot_settings){
+  
+  test <- readLines(cell_settings)
+  
+  vecs <- list()
+  mtnames <- c()
+  for (i in test){
+    if (!grepl("#", i)){
+      lbline = strsplit(i, "\t")
+      d = unlist(lbline)[[1]]
+      vecs <- append(vecs, lbline)
+      mtnames <- append(mtnames, d)    
+    }
+    
+  }
+  
+  pfx <- unique(gsub("_.*", "", mtnames[grep("_", mtnames)]))
+  pfx <- paste0(pfx, "_")
+  test <- list()
+  for (i in pfx){
+    test1 <- list(which(startsWith(mtnames, i)))
+    names(test1) = tolower(gsub("_", "", i))
+    test <- append(test, test1)
+  }
+  
+  sub_vecs <- list()
+  vec_names <- list()
+  for (i in test){
+    test_vec <- vecs[i]
+    sub_vecs <- append(sub_vecs, list(test_vec))
+  }
+  
+  # names(sub_vecs) <- vec_names[1:length(sub_vecs)]
+  names(sub_vecs) <- names(test)
+  
+  sub_vecs <- sub_vecs[unlist(lapply(sub_vecs, length) != 0)]
+  
+  param_dfs <- purrr::map2(sub_vecs, names(sub_vecs), match_cols)
+  
+  
+  
+  if (is.list(param_dfs) & length(param_dfs) != 0) {
+    param_dfs <- param_dfs %>%
+      Reduce(function(dtf1,dtf2) full_join(dtf1,dtf2,by="sample_id"), .) %>% 
+      arrange(sample_id)  
+    
+    dup_cells <- which(duplicated(param_dfs[,1]))
+    if (any(dup_cells)){
+      print(paste0("cells ", paste(param_dfs$sample_id[dup_cells], collapse = " "), " found duplicated in cell sets! They will be removed from analysis"))
+      param_dfs <- param_dfs[-dup_cells,]
+    }
+    
+    rownames(param_dfs) <- param_dfs[,1]
+    
+  }
+  
+  remove_cells <- find_remove_cells(plot_settings, param_dfs)
+  
+  
+  return(list("annotation" = param_dfs, "removed_cells" = remove_cells))
+  
+}
+
+take_input <- function(prompt = question, interactive = FALSE){
+  if(interactive){
+    param <- readline(prompt=question)
+  } else {
+    param <- readLines("stdin", n=1)
+  }
+  return(param)
+}
 
 lookup_genes <- function(txname){
   
@@ -64,41 +199,60 @@ lookup_transcripts <- function(genename){
   
 }
 
-plot_gene_by_treatment_and_day <- function(census_matrix, transcripts){
+plot_trx_by_treatment_and_facet <- function(transcript, annotation, facet){
+  filt_cm <- census_matrix[rownames(census_matrix) %in% transcript,]
+  filt_cm <- tidyr::gather(filt_cm, "sample_id", "counts") %>% 
+    inner_join(annotation)
+  new_levels <- mixedsort(levels(filt_cm[[facet]]))
+  filt_cm[[facet]] <- factor(filt_cm[[facet]], levels = new_levels)
+  bplot <- ggplot(data = filt_cm, aes(x=facet, y=counts)) + 
+    geom_boxplot() +
+    geom_jitter(height = 0, width = 0.1) +
+    # scale_x_discrete(mixedsort(levels(filt_cm[[facet]]))) +
+    facet_grid(. ~ treatment_group) + 
+    theme(axis.text.x=element_text(angle=90, hjust=1)) +
+    labs(title = lookup_genes(transcript), subtitle = transcript)
+  return(bplot)
+}
+
+plot_genes_summed_trx <- function(census_matrix, transcripts, annotation, facet){
   # input = readline(prompt="Enter gene symbol to plot (ex. MYCN): ")
-  
-  plot_transcript <- function(transcript){
-    filt_cm <- census_matrix[rownames(census_matrix) %in% transcript,]
-    filt_cm <- tidyr::gather(filt_cm, "sample_id", "counts") %>% 
-      inner_join(annotation)
-    new_levels <- mixedsort(levels(filt_cm$day))
-    filt_cm$day <- factor(filt_cm$day, levels = new_levels)
-    bplot <- ggplot(data = filt_cm, aes(x=day, y=counts)) + 
-      geom_boxplot() +
-      geom_jitter(height = 0, width = 0.1) +
-      # scale_x_discrete(mixedsort(levels(filt_cm$day))) +
-      facet_grid(. ~ treatment_group) + 
-      theme(axis.text.x=element_text(angle=90, hjust=1)) +
-      labs(title = lookup_genes(transcript), subtitle = transcript)
-    return(bplot)
-  }
-  
   # prep_transcripts <- lapply(transcripts, prep_transcript)
-  bplots <- lapply(transcripts, plot_transcript)
+  bplots <- lapply(transcripts, plot_trx_by_treatment_and_facet, annotation, facet)
   return(bplots)
 }
 
-question = "Choose from following:\n[P] Plot gene\n[L] Plot list of genes that are differentially expressed\n[X] Exit \n"
+# load data ---------------------------------------------------------------
 
-prompt_genes_names <- paste(basename(new_genes_names), collapse = " ")
+print("loading census matrix")
+census_matrix = cataract::safe_read(opt$expr_mat)
+print("loading cell metadata")
+annotation = cataract::safe_read(opt$annotation)
+
+mt_settings <- convert_mt_setting(opt$cellset, opt$plot_settings)
+
+if (!is.null(mt_settings$annotation)) {
+  exist_cols <- colnames(mt_settings$annotation)[!colnames(mt_settings$annotation) %in% colnames(annotation)]
+  exist_cols <- c("sample_id", exist_cols)
+  
+  mt_settings$annotation <- dplyr::select(mt_settings$annotation, exist_cols)
+  # mt_settings$annotation <- mt_settings$annotation[complete.cases(mt_settings$annotation),]
+  oldannotation <- as.data.frame(annotation)
+  names(oldannotation) <- tolower(names(oldannotation))
+  annotation <- left_join(oldannotation, mt_settings$annotation, by = "sample_id")
+}
+
 
 while (TRUE) {
-  # if (ctr < 3){
-  #   print(question)
-  # }
+  question = "Choose from following:\n
+                                    [P] Plot gene by treatment and day\n
+                                    [M] Plot gene by treatment and cluster\n
+                                    [L] Plot list of genes by treatment and day\n
+                                    [C] Plot list of genes by treatment and cluster\n
+                                    [X] Exit \n"
   
   cat(question)
-  action <- toupper(readLines("stdin",n=1))
+  action <- toupper(take_input(question))
   # action <- readline(prompt=question)
   
   if(action == "X"){
@@ -109,10 +263,25 @@ while (TRUE) {
     
     gene_question <- "Enter gene symbol to plot (ex. MYCN): "
     cat(gene_question)
-    input <- readLines("stdin", n=1)
+    input <- take_input(gene_question)
     transcripts <- lookup_transcripts(input)
     transcripts <- transcripts[which(transcripts %in% rownames(census_matrix))]
-    bplots <- plot_gene_by_treatment_and_day(census_matrix, transcripts) 
+    bplots <- plot_genes_summed_trx(census_matrix, transcripts, annotation, "day") 
+    pdf_out <- paste0(opt$out, "/", input, ".pdf")
+    pdf(pdf_out)
+    invisible(lapply(bplots, print))
+    dev.off()
+    print(paste0("saving ", pdf_out))
+  } else if (action == "M"){
+    
+    # plot gene expression across all 16 sets ---------------------------------
+    
+    gene_question <- "Enter gene symbol to plot (ex. MYCN): "
+    cat(gene_question)
+    input <- take_input(gene_question)
+    transcripts <- lookup_transcripts(input)
+    transcripts <- transcripts[which(transcripts %in% rownames(census_matrix))]
+    bplots <- plot_genes_summed_trx(census_matrix, transcripts, annotation, "cluster") 
     pdf_out <- paste0(opt$out, input, ".pdf")
     pdf(pdf_out)
     invisible(lapply(bplots, print))
@@ -123,17 +292,39 @@ while (TRUE) {
     treatment_question <- paste("Enter treatment_group to analyze (", prompt_genes_names, "): ")
     
     cat(treatment_question)
-    diffex_group <- readLines("stdin",n=1)
+    diffex_group <- take_input(treatment_question)
     # diffex_group <- readline(prompt=treatment_question)
     diffex_genes <- read.csv(new_genes_names[grep(diffex_group, new_genes_names)])
     
     top_n_question <- "how many genes do you want to plot? "
     cat(top_n_question)
-    top_n <- readLines("stdin", n=1)
+    top_n <- take_input(top_n_question)
     
     transcripts <- rownames(diffex_genes[c(1:top_n),])
     transcripts <- transcripts[which(transcripts %in% rownames(census_matrix))]
-    bplots <- plot_gene_by_treatment_and_day(census_matrix, transcripts) 
+    bplots <- plot_genes_summed_trx(census_matrix, transcripts, annotation, "day") 
+    pdf_out <- paste0(opt$out, diffex_group, ".pdf")
+    pdf(pdf_out)
+    invisible(lapply(bplots, print))
+    dev.off()
+    print(paste0("saving ", pdf_out))
+  } else if (action == "C"){
+    
+    treatment_question <- paste("Enter treatment_group to analyze (", prompt_genes_names, "): ")
+    
+    cat(treatment_question)
+    diffex_group <- take_input(treatment_question)
+    # diffex_group <- readline(prompt=treatment_question)
+    diffex_genes <- read.csv(new_genes_names[grep(diffex_group, new_genes_names)])
+    
+    top_n_question <- "how many genes do you want to plot? "
+    cat(top_n_question)
+    top_n <- take_input(top_n_question)
+    
+    transcripts <- rownames(diffex_genes[c(1:top_n),])
+    
+    transcripts <- transcripts[which(transcripts %in% rownames(census_matrix))]
+    bplots <- plot_genes_summed_trx(census_matrix, transcripts, annotation, "cluster") 
     pdf_out <- paste0(opt$out, diffex_group, ".pdf")
     pdf(pdf_out)
     invisible(lapply(bplots, print))
